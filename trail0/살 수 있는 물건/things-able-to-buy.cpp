a221132c3b325e1221c2e95f943ae1a68eccf302
SPh0052 <137913404+SPh0052@users.noreply.github.com>
2026-07-15 11:50:57 +0900

    [Passed] 살 수 있는 물건 (2ms, 0MB)

---
+++ trail0/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double N;
+
+    cin >> N;
+
+    if(N>=3000){
+        cout << "book";
+    }
+
+    else if(N>=1000){
+        cout << "mask";
+    }
+
+    else{
+        cout << "no";
+    }
+
+    return 0;
+}
