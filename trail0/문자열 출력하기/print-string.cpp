4d9460058846aa70cfced606ea48ed67a56cded0
SPh0052 <137913404+SPh0052@users.noreply.github.com>
2026-07-16 23:19:26 +0900

    [Wrong Answer] 문자열 출력하기 (2ms, 0MB)

---
+++ trail0/문자열 출력하기/print-string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    
+    cin >> N;
+
+    for(N=0; N<6; N++){
+        cout << "LeebrosCode" << "\n";
+    }
+    return 0;
+}
