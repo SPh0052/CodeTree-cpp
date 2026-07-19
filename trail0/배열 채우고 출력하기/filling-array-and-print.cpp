a92079436e2f99ad873b3a414a34e50428d3be60
SPh0052 <137913404+SPh0052@users.noreply.github.com>
2026-07-19 21:58:02 +0900

    [Passed] 배열 채우고 출력하기 (2ms, 0MB)

---
+++ trail0/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char array[10];
+
+    for(int i=0; i<10; i++){
+        cin >> array[i]; 
+    }
+
+    for(int i=9; i>=0; i--){
+        cout << array[i]; 
+    }
+    return 0;
+}
